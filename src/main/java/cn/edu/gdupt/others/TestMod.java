a7d5ebf1fa57ae9dcb153a1eb27e3c9f152a8722
Liang-Xiaobin <1490556728@qq.com>
2019-12-30 11:23:43 +0800

    添加列表和求余测试

---
+++ src/main/java/cn/edu/gdupt/others/TestMod.java
@@ -0,0 +1,16 @@
+package cn.edu.gdupt.others;
+
+/**
+ * @author Liang Xiaobin<1490556728@qq.com>
+ * @version 2019/12/9
+ * @since JDK1.8
+ */
+public class TestMod {
+    public static void main(String[] args) {
+        System.out.println((-10) % (-3));
+        System.out.println((-10) % 3);
+        System.out.println((10) % (-3));
+        System.out.println((10) % 3);
+        System.out.println((-20) % (3));
+    }
+}
